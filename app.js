cfca08fc374789eac8bf2fd2b70e4e6b231e97b7
Aziateur <143804218+Aziateur@users.noreply.github.com>
2024-12-28 19:18:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-// Simple test to see if React is working
+
 const e = React.createElement;
 
 const App = () => {
     return e('div', null, 'Testing React');
 };
 
-console.log('React:', !!window.React);  // See if React is available
-console.log('Root element:', !!document.getElementById('root')); // See if we can find root
+console.log('React:', !!window.React); 
+console.log('Root element:', !!document.getElementById('root')); 
 
-// Initialize the app
+
 ReactDOM.render(e(App), document.getElementById('root'));
